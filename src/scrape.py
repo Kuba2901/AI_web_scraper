a38d5e7c52dc8db8ca80a56aa3e68520144ba20e
Jakub Nenczak <kubanenczak@gmail.com>
2024-10-21 13:44:39 +0000

    Running in headless mode

--- src/scrape.py
+++ src/scrape.py
@@ -1,16 +1,23 @@
-from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver import Remote, ChromeOptions
+from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
 
 def scrape_website(website):
 	print("Launching chrome browser...")
-	options = webdriver.ChromeOptions()
-	driver = webdriver.Remote("http://chromium:4444/wd/hub", options=options)
-
-	try:
+	
+	chromium_remote_connection = ChromiumRemoteConnection("http://chromium:4444/wd/hub", 'goog', 'chrome')
+	options = ChromeOptions()
+	options.add_argument("--headless")
+	with Remote(chromium_remote_connection, options=options) as driver:
+		print(f"Connected! Navigating to {website}...")
 		driver.get(website)
-		print("Page loaded...")
-		html = driver.page_source
 
+		# TODO: Captcha handling
+		# print("Waiting for captcha to be solved...")
+		# solve_res = driver.execute('executeCdpCommand', {
+		# 	'cmd': "Captcha.waitForSolve",
+		# 	"params": {"detectTimeout": 10000},
+		# })
+		# print("Captcha solve status: " + solve_res['value']['status'])
+		# print("Navigated! Scraping page content...")
+		html = driver.page_source
 		return html
-	finally:
-		driver.quit()
